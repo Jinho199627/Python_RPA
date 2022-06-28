ffe4f6a6fee0347d63d7857eb8a1125abbe5184b
Jinho <j0418ok@naver.com>
2022-06-28 19:06:43 +0900

    1 commit

---
+++ 2_3_1_01.py
@@ -0,0 +1,13 @@
+# 파이썬 업무 자동화 ch 2의 3장
+
+import random
+# chars = "abcdegf"
+# print(random.choice(chars))
+
+import time
+# print(str(time.time())[-2:])
+## 무작위 숫자를 생성하는 방법
+
+import os
+os.listdir()
+# 폴더 안의 내용물을 리스트로 만들어준다.
